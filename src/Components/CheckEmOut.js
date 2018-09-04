e4d460b840e4fd66d24ee7e5d304a61312a3e93c
Drew Hackelman (dh694n) <dh694n@att.com>
2018-09-04 08:58:04 -0400

    Update portfolio to include Modal that tells user that project isn't ready for viewing quite yet

--- src/Components/CheckEmOut.js
+++ src/Components/CheckEmOut.js
@@ -2,9 +2,29 @@ import React, { Component } from 'react';
 import FaBasketball from 'react-icons/lib/fa/dribbble';
 import FaBomb from 'react-icons/lib/fa/bomb';
 import FaPhone from 'react-icons/lib/ti/phone-outline';
+import Modal from 'react-awesome-modal';
+
 
 
 class CheckEmOut extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible : false
+        }
+    }
+
+    openModal() {
+        this.setState({
+            visible : true
+        });
+    }
+
+    closeModal() {
+        this.setState({
+            visible : false
+        });
+    }
   render() {
     return (
       <div className='container_Projects'>
@@ -12,14 +32,27 @@ class CheckEmOut extends Component {
           <FaBasketball size={32} color={'black'} />
           <span className="titles">JerseyApp</span>
         </a>
-        <a href="" target="_blank">
+        <a href="javascript:void(0);" onClick={() => this.openModal()}>
           <FaBomb size={32} color={'black'} />
           <span className="titles">Ba-Bomb</span>
         </a>
-        <a href="" target="_blank">
+        <a href="javascript:void(0);" onClick={() => this.openModal()}>
           <FaPhone size={32} color={'black'} />
           <span className="titles">Chat-Tastic</span>
         </a>
+        <Modal 
+            visible={this.state.visible}
+            width="300"
+            height="200"
+            effect="fadeInUp"
+            onClickAway={() => this.closeModal()}
+        >
+            <div className="container_Modal">
+                <h1>Work In Progress</h1>
+                <p>Check again  in a couple days!</p>
+                <span onClick={() => this.closeModal()}>Close</span>
+            </div>
+        </Modal>
       </div>
     );
   }
